3678c18d5c50f205924220de6482d5da2303a1c6
Autuan <autuan.yu@gmail.com>
2018-03-09 08:37:59 +0800

    update index.html

--- blog/src/main/java/com/autuan/blog/controller/ShowController.java
+++ blog/src/main/java/com/autuan/blog/controller/ShowController.java
@@ -1,16 +1,26 @@
 package com.autuan.blog.controller;
 
+import com.autuan.blog.entity.Article;
+import com.autuan.blog.service.ArticleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class ShowController {
+    @Autowired
+    private ArticleService articleService;
+
     @RequestMapping("/")
     public ModelAndView toIndex(ModelAndView mav){
         mav.addObject("tempFile","tempFile.html");
         mav.setViewName("index");
 
+        List<Article> articles = articleService.getArticleList();
+        mav.addObject("articleList",articles);
         return mav;
     }
 }
