a19fa640aac1744e75d29db35a266a73c1d4e737
Autuan <autuan.yu@gmail.com>
2018-05-03 09:06:59 +0800

    friend link

---
+++ blog/src/main/java/com/autuan/blog/controller/FriendLinkController.java
@@ -0,0 +1,23 @@
+package com.autuan.blog.controller;
+
+
+import java.util.List;
+
+public class FriendLinkController {
+    // ------------------------- -------------------------
+    public List<?> getLinkList(){
+        return null;
+    }
+    public String updateLink(){
+        return null;
+    }
+    public String deleteLink(){
+        return null;
+    }
+    public String insertApplyLink(){
+        // 1.向数据库添加一条申请
+        // 2.向消息队列添加消息
+        return null;
+    }
+    //
+}
