62d7561a9b780bc2b5b7754f2f1398b375ab7af0
Autuan <autuan.yu@gmail.com>
2018-05-16 14:34:57 +0800

    add doc annotation

--- blog/src/main/java/com/autuan/blog/service/EmailService.java
+++ blog/src/main/java/com/autuan/blog/service/EmailService.java
@@ -1,6 +1,16 @@
 package com.autuan.blog.service;
 
 public interface EmailService {
+    /**
+     * email 发送
+     * @param aimAddress
+     * @param sendAddress
+     * @param smtpAddress
+     * @param emailUsername
+     * @param emailPassword
+     * @param messageTitle
+     * @param messageBody
+     */
     void sentHtmlEmail(String[] aimAddress,String sendAddress,String smtpAddress,
               String emailUsername,String emailPassword,String messageTitle,
               String messageBody);
